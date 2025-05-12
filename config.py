0304e728a8fee01e6e230f542acd44b3a60d0b25
taek105 <ht5172@gmail.com>
2025-05-12 18:47:46 +0900

    fix(config.py): 프롬프트 수정

--- config.py
+++ config.py
@@ -67,10 +67,9 @@ Or If the URL’s host contains 'coupang', return exactly:
 Otherwise, return exactly:
     parse_image_text
 
-DO NOT output anything else.
-DO NOT output any other text.
-DO NOT output any other format.
-Only output using TEXT"""
+DO NOT USE QUOTE.
+USE TEXT ONLY.
+"""
 
 PRODUCT_ANNC_PARCER_PROMPT = """
 다음은 HTML에서 텍스트만 추출한 상품 페이지 정보입니다.  
